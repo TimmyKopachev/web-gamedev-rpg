b83fbee2f7740444bd6508667a4fc95e43323ff3
ForgetEverythingAndRun <vik.kopachev@gmail.com>
2022-06-05 15:20:50 +0300

    JWT (filter & etc)

---
+++ forum/src/main/java/org/web/gamedev/rpg/forum/model/dto/JwtRequest.java
@@ -0,0 +1,22 @@
+package org.web.gamedev.rpg.forum.model.dto;
+
+public class JwtRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ forum/src/main/java/org/web/gamedev/rpg/forum/model/dto/JwtResponse.java
@@ -0,0 +1,17 @@
+package org.web.gamedev.rpg.forum.model.dto;
+
+public class JwtResponse {
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
