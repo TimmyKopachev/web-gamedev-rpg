6b13b0b4ce62f3ba7e5bd23de023763a4a2c5903
Timmy-PC <Warrior0076@yandex.ru>
2022-06-26 02:50:51 +0300

    fix lobby service

--- lobby/src/main/java/org/web/gamedev/rpg/lobby/web/LobbyRestController.java
+++ lobby/src/main/java/org/web/gamedev/rpg/lobby/web/LobbyRestController.java
@@ -2,23 +2,12 @@ package org.web.gamedev.rpg.lobby.web;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.web.gamedev.rpg.lobby.model.ReadyLobbyResponse;
-import org.web.gamedev.rpg.lobby.model.SearchingMatchmakingRequest;
 
 @Slf4j
 @RestController
 @AllArgsConstructor
 public class LobbyRestController {
 
-  @PostMapping
-  @RequestMapping("/find")
-  public ReadyLobbyResponse findMatchmaking(@RequestBody SearchingMatchmakingRequest request) {
 
-    return ReadyLobbyResponse.builder().build();
-  }
 }
