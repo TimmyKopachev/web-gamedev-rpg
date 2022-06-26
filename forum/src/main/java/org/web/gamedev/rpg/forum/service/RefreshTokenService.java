2002d9426c62edbaa90722a23504be1dc1329cfb
Timmy-PC <Warrior0076@yandex.ru>
2022-06-27 01:03:37 +0300

    refactoring

--- forum/src/main/java/org/web/gamedev/rpg/forum/service/RefreshTokenService.java
+++ forum/src/main/java/org/web/gamedev/rpg/forum/service/RefreshTokenService.java
@@ -1,5 +1,14 @@
 package org.web.gamedev.rpg.forum.service;
 
+import static org.web.gamedev.rpg.forum.config.JwtTokenUtil.REFRESH_TOKEN_EXPIRATION_IN_DAYS;
+import static org.web.gamedev.rpg.forum.config.JwtTokenUtil.SUBJECT_AUTH_REFRESH_TOKEN;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.web.gamedev.rpg.forum.config.JwtTokenUtil;
@@ -9,62 +18,66 @@ import org.web.gamedev.rpg.forum.model.entity.UserEntity;
 import org.web.gamedev.rpg.forum.repository.RefreshTokenRepository;
 import org.web.gamedev.rpg.forum.repository.UserRepository;
 
-import javax.transaction.Transactional;
-import java.time.*;
-import java.util.Date;
-import java.util.Optional;
-
-import static org.web.gamedev.rpg.forum.config.JwtTokenUtil.REFRESH_TOKEN_EXPIRATION_IN_DAYS;
-import static org.web.gamedev.rpg.forum.config.JwtTokenUtil.SUBJECT_AUTH_REFRESH_TOKEN;
-
 @Service
 @AllArgsConstructor
 public class RefreshTokenService {
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final UserRepository userRepository;
-    private final JwtTokenUtil jwtTokenUtil;
+  private final RefreshTokenRepository refreshTokenRepository;
+  private final UserRepository userRepository;
+  private final JwtTokenUtil jwtTokenUtil;
 
-    public Optional<RefreshTokenEntity> findById(Long id) {
-        return refreshTokenRepository.findById(id);
-    }
+  public Optional<RefreshTokenEntity> findById(Long id) {
+    return refreshTokenRepository.findById(id);
+  }
 
-    public Optional<RefreshTokenEntity> findByUserId(Long userId) {
-        return refreshTokenRepository.findByUserId(userId);
-    }
+  public Optional<RefreshTokenEntity> findByUserId(Long userId) {
+    return refreshTokenRepository.findByUserId(userId);
+  }
 
-    public Optional<RefreshTokenEntity> findByToken(String token) {
-        return refreshTokenRepository.findByToken(token);
-    }
+  public Optional<RefreshTokenEntity> findByToken(String token) {
+    return refreshTokenRepository.findByToken(token);
+  }
 
-    @Transactional
-    public RefreshTokenEntity createRefreshToken(Long userId) {
-        Instant issuedDate = LocalDateTime.now().toInstant(ZoneOffset.UTC);
-        Instant expiredDate = LocalDateTime.now().plusDays(REFRESH_TOKEN_EXPIRATION_IN_DAYS).toInstant(ZoneOffset.UTC);
-        RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity();
-        refreshTokenEntity.setUser(userRepository.findById(userId).get());
-        refreshTokenRepository.deleteByUserId(userId);
-        refreshTokenEntity.setIssuedDate(issuedDate);
-        refreshTokenEntity.setExpirationDate(expiredDate);
-        refreshTokenEntity.setToken(jwtTokenUtil.generateRefreshToken(Date.from(issuedDate), Date.from(expiredDate), SUBJECT_AUTH_REFRESH_TOKEN, userId.toString()));
-        refreshTokenEntity = refreshTokenRepository.save(refreshTokenEntity);
-        return refreshTokenEntity;
-    }
+  @Transactional
+  public RefreshTokenEntity createRefreshToken(Long userId) {
+    Instant issuedDate = LocalDateTime.now().toInstant(ZoneOffset.UTC);
+    Instant expiredDate =
+        LocalDateTime.now().plusDays(REFRESH_TOKEN_EXPIRATION_IN_DAYS).toInstant(ZoneOffset.UTC);
+    RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity();
+    refreshTokenEntity.setUser(
+        userRepository
+            .findById(userId)
+            .orElseThrow(
+                () ->
+                    new RuntimeException(String.format("Could not find user with ID %d", userId))));
+    refreshTokenRepository.deleteByUserId(userId);
+    refreshTokenEntity.setIssuedDate(issuedDate);
+    refreshTokenEntity.setExpirationDate(expiredDate);
+    refreshTokenEntity.setToken(
+        jwtTokenUtil.generateRefreshToken(
+            Date.from(issuedDate),
+            Date.from(expiredDate),
+            SUBJECT_AUTH_REFRESH_TOKEN,
+            userId.toString()));
+    refreshTokenEntity = refreshTokenRepository.save(refreshTokenEntity);
+    return refreshTokenEntity;
+  }
 
-    public RefreshTokenEntity verifyExpiration(RefreshTokenEntity token) {
-        if (token.getExpirationDate().isBefore(LocalDateTime.now()
-                .toInstant(ZoneOffset.UTC))) {
-            throw new TokenRefreshException(token.getToken(), TokenRefreshException.MESSAGE_REFRESH_TOKEN_EXPIRED);
-        }
-        return token;
+  public RefreshTokenEntity verifyExpiration(RefreshTokenEntity token) {
+    if (token.getExpirationDate().isBefore(LocalDateTime.now().toInstant(ZoneOffset.UTC))) {
+      throw new TokenRefreshException(
+          token.getToken(), TokenRefreshException.MESSAGE_REFRESH_TOKEN_EXPIRED);
     }
+    return token;
+  }
 
-    @Transactional
-    public void deleteByUserId(Long userId) {
-        Optional<UserEntity> userEntity = userRepository.findById(userId);
-        if (userEntity.isPresent()) {
-            refreshTokenRepository.deleteByUserId(userEntity.get().getId());
-            refreshTokenRepository.flush();
-        }
-        throw new IllegalArgumentException(String.format("There are no users with id: {} in DB.", userId));
+  @Transactional
+  public void deleteByUserId(Long userId) {
+    Optional<UserEntity> userEntity = userRepository.findById(userId);
+    if (userEntity.isPresent()) {
+      refreshTokenRepository.deleteByUserId(userEntity.get().getId());
+      refreshTokenRepository.flush();
     }
+    throw new IllegalArgumentException(
+        String.format("There are no users with id: [%d] in DB.", userId));
+  }
 }
